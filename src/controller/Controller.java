d15f45c4ec7413a99acd261d916b63bbb7e05f1b
niek <niek1998@live.com>
2017-03-20 12:13:13 +0100

    View gemaakt +Vincent

---
+++ src/controller/Controller.java
@@ -0,0 +1,36 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+/**
+ * Created by niek on 20-3-17.
+ */
+public class Controller {
+    @FXML private Pane rootPane;
+    @FXML private GridPane bogglePane;
+    @FXML private Button manualButton;
+    @FXML private Button randomButton;
+    @FXML private TextField inputArea;
+    @FXML private TextArea foundWordsArea;
+
+    public Controller(){}
+
+    @FXML
+    private void initialize(){}
+
+    @FXML
+    private void inputAction(){
+
+    }
+
+    @FXML
+    private void randomInput(){
+
+    }
+
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,31 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main extends Application{
+
+    //String fxmlDocPath = "/home/niek/Bureaublad/Nieksort/src/layout.fxml";
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        String fxmlDocPath = "/home/niek/IdeaProjects/Inleveropdracht2-VincentLuder-NiekBeukema-ITV2E/src/view/layout.fxml";
+        FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+        Pane rootPane = loader.load(fxmlStream);
+        Scene scene = new Scene(rootPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Boggle Geval");
+        primaryStage.show();
+    }
+}
