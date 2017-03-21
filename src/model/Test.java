13b13025d845199496f3ce3a9c5114289f52afeb
redvox27 <v.t.luder@st.hanze.nl>
2017-03-21 20:50:53 +0100

    test toegevoegd samen met methoden om uit te bouwen samen met niek

---
+++ src/model/Test.java
@@ -0,0 +1,94 @@
+package model;
+
+/**
+ * Created by Gebruiker on 21-3-2017.
+ */
+public class Test {
+    private String[] woorden = {"hoi","kak","boi"};
+    private String[][] array = new String[3][3];
+    private ArrayFiller filler = new ArrayFiller();
+
+    public Test(){
+        filler.fillArray(array);
+    }
+
+    public void searchWord(String[][] array){
+        this.array = array;
+
+        int numberOfRows = array.length;
+        int numberOfColumns = array[0].length;
+        // Check rows
+
+        String test = "";
+
+        //horizontale search
+        for(int i = 0; i< numberOfRows;i++){
+            for(int j =0; j < numberOfColumns; j++){
+                test += array[0][j];
+            }
+        }
+        System.out.println(test);
+
+
+    }
+
+    public String[][] getArray(){
+        return array;
+    }
+
+    public static void isConsecutiveFour(int[][] values) {
+        int numberOfRows = values.length;
+        int numberOfColumns = values[0].length;
+
+        // Check rows
+        int rowCheck;
+        int columnCheck;
+        int counter = 1;
+        int temp;
+
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                rowCheck = values[i][j];
+                temp = values[i][j - 1];
+
+                if (rowCheck == temp) {
+                    counter++;
+                    if (counter == 4) {
+                        System.out.println(" match found");
+                    }
+                } else {
+                    counter = 1;
+                }
+
+            }
+        }
+        for (int i = 0; i < numberOfColumns; i++) {
+            for (int j = 1; j < numberOfRows; j++) {
+                rowCheck = values[j][i];
+                temp = values[j - 1][i];
+
+                if (rowCheck == temp) {
+                    counter++;
+                    if (counter == 4) {
+                        System.out.println(" match found");
+                    }
+                } else {
+                    counter = 1;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        for(int i = 0; i < test.getArray().length; i++){
+            for (int j =0; j<test.getArray().length;j++){
+                System.out.println(test.getArray()[i][j]);
+            }
+        }
+
+
+        test.searchWord(test.getArray());
+    }
+}
